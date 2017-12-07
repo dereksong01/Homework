9db2cf5905bab77c18f42bdec0885f843a78437d
Derek Song <dereksong@Haes-Air.home>
2017-12-06 23:46:38 -0500

    hiya

---
+++ GuessNumber.java
@@ -0,0 +1,15 @@
+// Derek Song
+// APCS1 - pd1
+// HW#48 - Keep Guessing
+// 2017 - 12 - 6
+
+import cs1.Keyboard;
+
+public class GuessNumber {
+
+    // instance vars
+    int high;
+    int low;
+
+    
+}
