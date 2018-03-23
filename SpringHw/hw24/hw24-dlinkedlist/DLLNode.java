62ca01905058d01e3c9d3db612b8224e8a42ac48
Derek Song <dsong1@stuy.edu>
2018-03-23 14:08:06 -0400

    hw24

--- SpringHw/hw24/hw24-dlinkedlist/DLLNode.java
+++ SpringHw/hw24/hw24-dlinkedlist/DLLNode.java
@@ -7,9 +7,46 @@
 
 public class DLLNode 
 {
-  private String _cargo;    //cargo may only be of type String
-  private DLLNode _nextNode, _prevNode; //pointers to next, prev DLLNodes
+    private String _cargo;    //cargo may only be of type String
+    private DLLNode _nextNode, _prevNode; //pointers to next, prev DLLNodes
 
-  //YOUR IMPLEMENTATION HERE...
+    public DLLNode( String value, DLLNode next ) {
+	_cargo = value;
+	_nextNode = next;
+    }
 
+
+    //--------------v  ACCESSORS  v--------------
+    public String getCargo() { return _cargo; }
+
+    public DLLNode getNext() { return _nextNode; }
+
+    public DLLNode getPrev() { return _prevNode; }
+    //--------------^  ACCESSORS  ^--------------
+
+
+    //--------------v  MUTATORS  v--------------
+    public String setCargo( String newCargo ) {
+	String foo = getCargo();
+	_cargo = newCargo;
+	return foo;
+    }
+
+    public DLLNode setNext( DLLNode newNext ) {
+	DLLNode foo = getNext();
+	_nextNode = newNext;
+	return foo;
+    }
+
+    public DLLNode setPrev( DLLNode newPrev) {
+	DLLNode foo = getPrev();
+	_prevNode = newPrev;
+	return foo;
+    }
+    //--------------^  MUTATORS  ^--------------
+
+    
+    // override inherited toString
+    public String toString() { return _cargo.toString(); }
+    
 }//end class DLLNode
